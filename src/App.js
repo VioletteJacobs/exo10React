4863c0512a9c5fdcaea8a0143ce9693803afa2d1
Violette Jacobs <violettejacobs@MacBook-Pro-de-Violette.local>
2021-01-19 09:39:55 +0100

    setState

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import './App.css';
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state={
+    titre:"heyho",
+    paragraphe:"coucou zuehdkbehzdkjhazbef"
+  }
+  changeTitle=()=>{
+    this.setState({titre: "bouloulou"});
+  }
+
   render() {
     return (
       <div>
-        <h1>Hey</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At eius fuga, nostrum enim odit totam.</p>
-        <button>Changez tout ici</button>
+        <h1 onClick={()=>{this.changeTitle()}}>{this.state.titre}</h1>
+
+        <p className="para">{this.state.paragraphe}</p>
+
+        <button onClick = {() =>
+        {
+          this.setState({paragraphe:"coucou liehrfkzhberf"})
+
+        }} >Changez le paragraphe ici</button>
       </div>
     )
   }
